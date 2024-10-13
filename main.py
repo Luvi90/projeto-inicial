c74937964c7fa2639464e392499561e4a86ac8e3
Thiago Luvisotto <thiago.luvisotto@gmail.com>
2024-10-13 16:05:18 -0300

    Meu primeiro commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Jornada de Dados")
+
+# Variáveis
+idade = 34
+nome = "Thiago"
+
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4]
+
+# Métodos
+
+#lista_invertida = lista.remove()
